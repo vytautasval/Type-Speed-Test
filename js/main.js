b0def79d21903173e9f05e49302120dc29432b81
Vytautas Valiulis <vytautasval40@gmail.com>
2023-11-04 18:43:50 +0200

    highlight done

--- js/main.js
+++ js/main.js
@@ -6,6 +6,13 @@ const userAnswer = document.getElementById('user-answer')
 const testText = document.getElementById('test-text')
 const highlight = document.getElementById('highlight')
 
+let totalCorrectAnswers = []
+let totalUserAnswers = []
+let wordCorrectness = []
+
+let correctAnswers = 0
+let incorrectAnswers = 0
+
 /**Starts a 60 sec countdown and updates the innerHTMl every 1 sec.*/
 function startTimer() {
     timer = setInterval(() => {
@@ -30,19 +37,31 @@ async function initLine() {
 function colorReact() { 
     const userText = userAnswer.value
     const testTextContent = testText.textContent   
+    const testTextArray = testTextContent.split(' ')
     let coloredText = ''
+    let correct = true
+
     for (let i = 0; i < Math.min(userText.length, testTextContent.length); i++) {
         if (userText.charAt(i) === testTextContent.charAt(i)) {
             coloredText += '<span style="background-color: #66FF99;">' + testTextContent.charAt(i) + '</span>'
         } else {
             coloredText += '<span style="background-color: #FFCCCB;">' + testTextContent.charAt(i) + '</span>'    
-        }
-           
+            correct = false
+        }           
     }
     if (testTextContent.length > userText.length) {
         coloredText += testTextContent.substring(userText.length)
     } 
     testText.innerHTML = coloredText
+    
+    for (let i = 0; i < testTextArray.length; i++) {
+        if (i < userText.length && userText.charAt(i) === testTextArray[i]) {
+            wordCorrectness[i] = true;
+        } else {
+            wordCorrectness[i] = false;
+        }
+        }    
+    
 }
 
 function currentWord() {
@@ -53,14 +72,25 @@ function currentWord() {
     let currentWordIndex = 0  
     
     for (let i = 0; i < userText.length; i++) {
-      if (i < (testTextArray[currentWordIndex].length + totalLengthBase)) {
+        let adjustedWordPos = testTextArray[currentWordIndex].length + totalLengthBase
+      if (i < adjustedWordPos) {
         highlight.innerHTML = testTextArray[currentWordIndex]        
-      } else {
+      } else {        
         totalLengthBase += testTextArray[currentWordIndex].length + 1
         currentWordIndex++
-      } 
-      
+      }      
     }    
+}
+function computeStats() {
+    for (let i = 0; i < wordCorrectness.length; i++) {
+        if (wordCorrectness[i]) {
+            correctAnswers++
+        } else {
+            incorrectAnswers++
+        }
+    }
+
+    wordCorrectness = []
 }
 
 
@@ -70,6 +100,12 @@ userAnswer.addEventListener('input', () => {
     }
     currentWord()    
     colorReact()
+    if (userAnswer.value.length === testText.textContent.length) {
+        computeStats()
+        document.getElementById('stats').innerHTML = `+${correctAnswers}, -${incorrectAnswers}`
+        userAnswer.value = ''
+        initLine()
+    }
     
 })
 
